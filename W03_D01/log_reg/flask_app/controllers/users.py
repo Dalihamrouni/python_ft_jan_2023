d29c67a148e2017ec46d8044d806a4577c21a3fd
John Doe <johndoe@example.com>
2023-02-13 16:33:44 +0100

    Login & Registration + Validations

--- W03_D01/log_reg/flask_app/controllers/users.py
+++ W03_D01/log_reg/flask_app/controllers/users.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask_app.models.user import User
-from flask import render_template, redirect, request, session
+from flask import render_template, redirect, request, session, flash
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 # ============= Index Page=================
@@ -20,5 +20,39 @@ def register():
             'email': request.form['email'],
             'password': hashed_password,
         }
-        User.create_user(data)
+        user_id = User.create_user(data)
+        print("+"*20, user_id,"+"*20)
+        session['user_id'] = user_id
+        return redirect('/dashboard')
+    return redirect('/')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id'  not in session:
+        return redirect('/')
+    logged_user = User.get_by_id({'id':session['user_id']})
+    return render_template("dashboard.html", logged_user = logged_user)
+
+@app.route('/users/login', methods=['post'])
+def login():
+    # Verify Email
+    data = {
+        'email': request.form['email']
+    }
+    user_from_db = User.get_by_email(data)
+    print("*"*20,user_from_db,"*"*20)
+    if user_from_db:
+        if not bcrypt.check_password_hash(user_from_db.password, request.form['password']):
+        # if we get False after checking the password
+            flash("Invalid Password","login")
+            return redirect('/')
+        else:
+            session['user_id'] = user_from_db.id
+            return redirect("/dashboard")
+    flash("Invalid Email","login")
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.clear()
     return redirect('/')
--- W03_D01/log_reg/flask_app/models/user.py
+++ W03_D01/log_reg/flask_app/models/user.py
@@ -1,5 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import DATABASE
+from flask import flash
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
@@ -19,6 +20,16 @@ class User:
          """
          return connectToMySQL(DATABASE).query_db(query,data)
     @classmethod
+    def get_by_id(cls, data):
+        query = """
+        SELECT * FROM users WHERE  id = %(id)s;
+        """
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        if len(results) <1 :
+            return False
+        return cls(results[0])
+    
+    @classmethod
     def get_by_email(cls, data):
         query = """
         SELECT * FROM users WHERE  email = %(email)s;
@@ -28,18 +39,22 @@ class User:
             return False
         return cls(results[0])
 
-    # *=============V= VALIDATIONS ===============
+    # *============== VALIDATIONS ===============
     @staticmethod
     def validate(data):
         is_valid = True
         if len(data['first_name'])<2:
             is_valid = False
+            flash("first name is required !!!","reg")
         if len(data['last_name'])<2:
             is_valid  = False
+            flash("last name is required !!!","reg")
         if not EMAIL_REGEX.match(data['email']): 
             is_valid = False
+            flash("Email is required!!!")
         elif User.get_by_email({"email":data['email']}):
             is_valid = False
+            flash("Email already taken Hopefully  by you!!!","reg")
         # else:
         #     user =   User.get_by_email({"email":data['email']})
         #     print(user)
@@ -47,7 +62,9 @@ class User:
         #         is_valid = False
         if len(data['password'])<7:
             is_valid = False
+            flash("Password is required !!!" ,"reg")
         elif data['password'] != data['confirm_password']:
             is_valid = False
+            flash("Passwords must match !!!","reg")
         return is_valid
         
